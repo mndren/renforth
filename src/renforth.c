ea5878490d59a5a45af0397cdac99179b938782f
renè <rene.menditto@libero.it>
2025-10-26 23:19:29 +0100

    .

--- src/renforth.c
+++ src/renforth.c
@@ -1,13 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-    char s[]="";
-    printf("Renforth Interpreter (0.0.1) \n");
-    printf("<< ");
-    scanf("%s",s);
-    printf("%s \n", s);
-     
+    char input[256];
+
+    printf("Renforth Interpreter (0.0.1)\n");
+
+    while (1) {
+        printf("<< ");
+
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            printf("\nEOF detected. Exiting...\n");
+            break;
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strcmp(input, "exit") == 0) {
+            break;
+        }
+
+        printf("%s\n", input);
+    }
 
     return 0;
 }
