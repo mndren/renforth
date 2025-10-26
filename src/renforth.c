b845a9e5133d8cb197d1b66a642628b1c2abdbb6
renè <rene.menditto@libero.it>
2025-10-26 23:11:59 +0100

    init

---
+++ src/renforth.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    char s[]="";
+    printf("Renforth Interpreter (0.0.1) \n");
+    printf("<< ");
+    scanf("%s",s);
+    printf("%s \n", s);
+     
+
+    return 0;
+}
